5a1e9fc7f39b58890a6ac9cb799da7a95b37ff31
Monkir Chowdhury <monkirchowdhury@gmail.com>
2023-05-10 23:13:59 +0600

    tailwind, flowbite, tailwind element

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+
+import "tw-elements/dist/css/tw-elements.min.css";
+import 'flowbite';
+import "@/styles/globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
